3a28eafbcf8ce66f9a851e8c221a20ddf77d64ad
Justin McBride <jstnmcbrd@outlook.com>
2024-01-12 12:44:40 -0800

    Add Vite+React template (#1)

---
+++ eslint.config.js
@@ -0,0 +1,90 @@
+import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import reactPlugin from 'eslint-plugin-react';
+import globals from 'globals';
+
+export default [
+	{
+		ignores: ['dist'],
+	},
+	{
+		// All JavaScript-esque files
+		files: ['**/*.{m,c,}{js,ts}{x,}'],
+		plugins: {
+			'@stylistic': stylistic,
+		},
+		languageOptions: {
+			sourceType: 'module',
+			ecmaVersion: 'latest',
+			globals: {
+				...globals.node,
+				...globals.es2021, // es2022 is not available (https://github.com/sindresorhus/globals/issues/183)
+			},
+		},
+		rules: {
+			// Recommended
+			...js.configs.recommended.rules,
+			...stylistic.configs['recommended-flat'].rules,
+
+			// Overrides
+			'@stylistic/indent': ['error', 'tab'],
+			'@stylistic/no-tabs': 0,
+			'@stylistic/semi': ['error', 'always'],
+
+			// Additions
+			'curly': 'error',
+			'no-shadow': 'error',
+			'no-var': 'error',
+			'prefer-const': 'error',
+		},
+	},
+	{
+		// TypeScript files
+		files: ['**/*.{m,c,}ts{x,}'],
+		plugins: {
+			'@typescript-eslint': typescriptPlugin,
+		},
+		languageOptions: {
+			parser: typescriptParser,
+			parserOptions: {
+				project: './tsconfig.eslint.json',
+			},
+		},
+		rules: {
+			...typescriptPlugin.configs['strict-type-checked'].rules,
+			...typescriptPlugin.configs['stylistic-type-checked'].rules,
+		},
+	},
+	{
+		// React files
+		files: ['**/*.{m,c,}{js,ts}x'],
+		plugins: {
+			react: reactPlugin,
+		},
+		languageOptions: {
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+			globals: {
+				...globals.browser,
+			},
+		},
+		settings: {
+			react: {
+				version: 'detect',
+			},
+		},
+		rules: {
+			// Recommended
+			...reactPlugin.configs.recommended.rules,
+			...reactPlugin.configs['jsx-runtime'].rules,
+
+			// Overrides
+			'@stylistic/jsx-indent': ['error', 'tab'],
+		},
+	},
+];
---
+++ src/vite-env.d.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line @stylistic/spaced-comment
+/// <reference types="vite/client" />
