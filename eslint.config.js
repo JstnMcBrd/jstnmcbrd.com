ef539c05493cf510f8ce6e18ed3e9ae24fe8ada2
Justin McBride <jstnmcbrd@outlook.com>
2024-01-19 19:11:27 -0700

    Turn off no-undef for TypeScript

--- eslint.config.js
+++ eslint.config.js
@@ -59,6 +59,8 @@ export default [
 		rules: {
 			...typescriptPlugin.configs['strict-type-checked'].rules,
 			...typescriptPlugin.configs['stylistic-type-checked'].rules,
+
+			'no-undef': 0, // handled by TypeScript
 		},
 	},
 	{
