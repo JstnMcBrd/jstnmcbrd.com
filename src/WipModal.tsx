ed76e23bbf2324df23e053ff231bc7158e4b8a6c
Justin McBride <jstnmcbrd@outlook.com>
2024-03-19 16:26:11 -0600

    Remove unnecessary WipModal nesting

--- src/WipModal.tsx
+++ src/WipModal.tsx
@@ -11,18 +11,16 @@ export function WipModal(): JSX.Element {
 	}
 
 	return (
-		<>
-			<Modal centered show={show} onHide={handleClose} className="text-center">
-				<Modal.Header closeButton>
-					<Container className="ms-3">
-						<Modal.Title>⚠️ Work-in-progress ⚠️</Modal.Title>
-					</Container>
-				</Modal.Header>
-				<Modal.Body>
-					<p>This website is not finished!</p>
-					<p>{'Please don\'t judge too harshly :)'}</p>
-				</Modal.Body>
-			</Modal>
-		</>
+		<Modal centered show={show} onHide={handleClose} className="text-center">
+			<Modal.Header closeButton>
+				<Container className="ms-3">
+					<Modal.Title>⚠️ Work-in-progress ⚠️</Modal.Title>
+				</Container>
+			</Modal.Header>
+			<Modal.Body>
+				<p>This website is not finished!</p>
+				<p>{'Please don\'t judge too harshly :)'}</p>
+			</Modal.Body>
+		</Modal>
 	);
 }
