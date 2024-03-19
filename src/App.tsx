0c8ce0967f870f9ac6e8a5b99d91153b3a84f613
Justin McBride <jstnmcbrd@outlook.com>
2024-03-19 16:25:41 -0600

    Add Konami Code easter egg

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import type { JSX } from 'react';
 
 import { Header } from './header/Header.js';
 import { Content } from './Content.js';
-import { WipModal } from './WipModal.js';
 import { Footer } from './Footer.js';
+import { Konami } from './Konami.js';
+import { WipModal } from './WipModal.js';
 
 export function App(): JSX.Element {
 	return (
 		<>
 			<WipModal />
+			<Konami />
 			<Header />
 			<Content />
 			<Footer />
---
+++ src/Konami.tsx
@@ -0,0 +1,71 @@
+import type { JSX } from 'react';
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+
+/** https://en.wikipedia.org/wiki/Konami_Code */
+const konamiSequence = [
+	'ArrowUp',
+	'ArrowUp',
+	'ArrowDown',
+	'ArrowDown',
+	'ArrowLeft',
+	'ArrowRight',
+	'ArrowLeft',
+	'ArrowRight',
+	'b',
+	'a',
+	'Enter',
+];
+
+function onKonami(callback: (event?: KeyboardEvent) => void): void {
+	let sequenceIndex = 0;
+	window.addEventListener('keydown', function (event: KeyboardEvent): void {
+		if (event.key === konamiSequence[sequenceIndex]) {
+			sequenceIndex++;
+		}
+		// Reset the sequence if the user gets one wrong
+		else {
+			sequenceIndex = 0;
+		}
+
+		// Success
+		if (sequenceIndex === konamiSequence.length) {
+			callback(event);
+			sequenceIndex = 0;
+		}
+	});
+}
+
+/** A Konami code easter egg that plays the Rick Roll video when triggered. */
+export function Konami(): JSX.Element {
+	const rickRollVideoId = 'dQw4w9WgXcQ';
+
+	const [show, setShow] = useState(false);
+
+	function handleClose() {
+		setShow(false);
+	}
+
+	onKonami(() => {
+		setShow(!show);
+	});
+
+	return (
+		<Modal size="xl" centered show={show} onHide={handleClose}>
+			<Modal.Body
+				style={{
+					aspectRatio: '16/9',
+					maxHeight: '90vh', // 100vh causes a scrollbar to appear
+				}}
+			>
+				<iframe
+					title="Rick Roll"
+					width="100%"
+					height="100%"
+					src={`https://www.youtube.com/embed/${rickRollVideoId}?autoplay=1`}
+					allow="autoplay"
+				/>
+			</Modal.Body>
+		</Modal>
+	);
+}
